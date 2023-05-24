002ca8fe81dbca3fe41554c3c72c8683fcbff880
LucasMisk <lucasmis55@gmail.com>
2023-05-24 18:33:00 +0300

    Cache Score

--- src/Score/CacheScore.java
+++ src/Score/CacheScore.java
@@ -1,7 +1,5 @@
 package Score;
 
-import static java.lang.Math.cbrt;
-
 public class CacheScore implements iScore{
     @Override
     public int getScore() {
@@ -12,6 +10,6 @@ public class CacheScore implements iScore{
     public int getScore(Object... params) {
         long time = (long)params[0];
         double time1 = time/1000000000.0;
-        return (int)(cbrt(40000/time1)*1000);
+        return (int)(40000/time1);
     }
 }
