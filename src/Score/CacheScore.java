d4d22b3c2e6cd46066404813fc993236f4eaa26b
LucasMisk <lucasmis55@gmail.com>
2023-05-24 18:22:17 +0300

    Cache Score

--- src/Score/CacheScore.java
+++ src/Score/CacheScore.java
@@ -12,6 +12,6 @@ public class CacheScore implements iScore{
     public int getScore(Object... params) {
         long time = (long)params[0];
         double time1 = time/1000000000.0;
-        return (int)cbrt(40000/time1)*1000;
+        return (int)(cbrt(40000/time1)*1000);
     }
 }
